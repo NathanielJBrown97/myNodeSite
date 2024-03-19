781e4226cb2205a3c633da8cf95392e2f4233386
Nathaniel <nathaniel.j.brown.97@gmail.com>
2024-03-19 14:31:56 -0400

    initiating new repository for this assignment

---
+++ data/tempAjaxLoader.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    // Load HTML content
+    $("#loadHtmlBtn").click(function() {
+        clearContent(); // clear any existing content
+        $("#htmlContent").load("../../data/tempContent.html");
+    });
+
+    // Load and process JSON data
+    $("#loadJsonBtn").click(function() {
+        clearContent(); // clear any existing content
+        $.getJSON("../../data/tempContent.json", function(data) {
+            var items = [];
+            items.push("<h3>" + data.title + "</h3><p>" + data.description + "</p>");
+            $.each(data.links, function(index, link) {
+                items.push("<a href='" + link.href + "'>" + link.title + "</a><br>");
+            });
+            $("#jsonContent").html(items.join("")); 
+        });
+    });
+
+    // Load HTML content with jQuery
+    $("#loadJqueryContentBtn").click(function() {
+        clearContent(); // clear any existing content
+        $.ajax({
+            url: "../../data/tempJqueryContent.html",
+            success: function(result) {
+                $("#jqueryContent").html(result); 
+            }
+        });
+    });
+
+    // Function to clear previous content
+    function clearContent() {
+        $("#htmlContent").empty();
+        $("#jsonContent").empty();
+        $("#jqueryContent").empty();
+    }
+});
